03c3c1fdd8859e0587fa6fea1447cb450d7fcc6f
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-11-15 13:20:18 +0100

    SaveAndEditFunctionality
    Not tested yet

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,4 +1,7 @@
 ﻿using Mono.Data.Sqlite;
+using System;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEditor.U2D.Common;
 
 public class CardManager : DatabaseHandler
@@ -8,7 +11,7 @@ public class CardManager : DatabaseHandler
 
     public static CardManager Instance
     {
-        get 
+        get
         {
             if (instance == null)
             {
@@ -18,43 +21,108 @@ public class CardManager : DatabaseHandler
         }
     }
 
-   
 
-    protected Card FindFromDb(string cardName, int ID)
+    /// <summary>
+    /// Returns a Card object that matches the Name of the card and the Deck ID
+    /// </summary>
+    /// <param name="cardName"></param>
+    /// <param name="deckID"></param>
+    /// <returns></returns>
+    protected Card FindFromDb(string cardName, int deckID)
     {
 
         Open();
 
-        var cmd = new SqliteCommand($"SELECT Title, Type, Tag, Description, Icon FROM 'Card' WHERE DeckID={ID} AND Title={cardName}", (SqliteConnection)connection);
+        var cmd = new SqliteCommand($"SELECT Title, Type, Tag, TagText, Description, Icon, Sprite " +
+            $"FROM Card" +
+            $" WHERE DeckID='{deckID}' " +
+            $"AND Title='{cardName}'",
+            (SqliteConnection)connection);
+
         var dataRead = cmd.ExecuteReader();
 
-
+        //retrieve all columns from database
         string title = dataRead.GetString(0);
         string type = dataRead.GetString(1);
         int tag = dataRead.GetInt32(2);
-        string description = dataRead.GetString(3);
-        int icon = dataRead.GetInt32(4);
+        string tagText = dataRead.GetString(3);
+        string description = dataRead.GetString(4);
+        string icon = dataRead.GetString(5);
+        string sprite = dataRead.GetString(6);
 
-        //  Card card = new Card(title, description, type, tag, icon);
-        Card card = new Card();
+        //divide into strings using commas
+        string[] iconSplit = icon.Split(',');
+        int[] iconValues = new int[iconSplit.Length];
 
-        currentCard = card;
+
+        for (int i = 0; i < iconSplit.Length; i++)
+        {
+            //Convert to integer
+            iconValues[i] = int.Parse(iconSplit[i]);
+        }
+
+        Card card = new Card(title, description, type, (TagType)tag, tagText, iconValues, sprite) { SpritePath = sprite, iconValues = iconValues };
+
 
         return card;
     }
 
-    public void EditCard(Card? card)
+    /// <summary>
+    /// Deletes a card from the matching Deck and inserts a new one.
+    /// </summary>
+    /// <param name="deckID"></param>
+    /// <param name="cardName"></param>
+    /// <param name="updatedCard"></param>
+    public void EditCard(int deckID, string cardName, Card updatedCard)
     {
+        Open();
 
+        var cmd = new SqliteCommand($"DELETE FROM Card WHERE DeckID={deckID} AND Name={cardName}", (SqliteConnection)connection);
+        cmd.ExecuteNonQuery();
+
+        cmd = new SqliteCommand($"INSERT INTO Card VALUES " +
+            $"('null'," +
+            $"'{deckID}'," +
+            $"'{updatedCard.titleText}'," +
+            $"'{updatedCard.typeText}'," +
+            $"'{(int)updatedCard.TType}', " +
+            $"'{updatedCard.tagText}', " +
+            $"'{updatedCard.descriptionText}'," +
+            $"'{updatedCard.iconValues}'," +
+            $"'{updatedCard.SpritePath}'",
+            (SqliteConnection)connection);
+
+        cmd.ExecuteNonQuery();
+
+        Close();
     }
 
-    public void SaveToDb(int ID)
+
+    /// <summary>
+    /// Saves card to the database with the matching deck
+    /// </summary>
+    /// <param name="deckId"></param>
+    /// <param name="card"></param>
+    public void SaveToDb(int deckId, Card card)
     {
-        if (currentCard != null)
-        {
-            var cmd = new SqliteCommand($"INSERT INTO Card (ID, DeckID, Title, Type, Tag, Description, Icon" +
-                $"VALUES '{currentCard.ID}','{ID}', '{currentCard.titleText}'");//mangler resten men der er ikke enums og properties endnu
-        }
+        Open();
+
+        var cmd = new SqliteCommand($"INSERT INTO Card (ID, DeckID, Title, Type, Tag, TagText, Description, Icon, Sprite VALUES " +
+            $"('null', " +
+            $" '{deckId}', " +
+            $" '{card.titleText}', " +
+            $" '{card.typeText}'," +
+            $" '{(int)card.TType}', " +
+            $" '{card.tagText}'," +
+            $" '{card.descriptionText}'," +
+            $" '{card.iconValues}'," +
+            $" '{card.SpritePath}'",
+            (SqliteConnection)connection);
+
+        cmd.ExecuteNonQuery();
+
+        Close();
+
     }
 
 
---
+++ Assets/Scripts/DeckManager.cs
@@ -0,0 +1,74 @@
+﻿using Mono.Data.Sqlite;
+using UnityEditor.MemoryProfiler;
+
+public class DeckManager : DatabaseHandler
+{
+    private static DeckManager instance;
+    private Deck currentDeck;
+
+    public static DeckManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new DeckManager();
+            }
+            return instance;
+        }
+    }
+
+
+
+    /// <summary>
+    /// Loads the deck from database with the corresponding ID
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <returns></returns>
+    public Deck LoadDeck(int ID)
+    {
+        Deck deck = new Deck();
+        Open();
+
+        var cmd = new SqliteCommand($"SELECT Title, Type, Tag, TagText, Description, Icon, Sprite FROM 'Card' WHERE DeckID={ID}", (SqliteConnection)connection);
+        var dataRead = cmd.ExecuteReader();
+
+        while (dataRead.Read())
+        {
+            string title = dataRead.GetString(0);
+            string type = dataRead.GetString(1);
+            int tag = dataRead.GetInt32(2);
+            string tagText = dataRead.GetString(3);
+            string description = dataRead.GetString(4);
+            string icon = dataRead.GetString(5);
+            string sprite = dataRead.GetString(6);
+
+            string[] iconSplit = icon.Split(',');
+            int[] iconValues = new int[iconSplit.Length];
+
+            for (int i = 0; i < iconSplit.Length; i++)
+            {
+                iconValues[i] = int.Parse(iconSplit[i]);
+            }
+
+            deck.cards.Add(new Card(title, description, type, (TagType)tag, tagText, iconValues, sprite) { iconValues = iconValues, SpritePath = sprite});
+
+        }
+
+        Close();
+        currentDeck = deck;
+        return deck;
+
+    }
+
+    public void DeleteDeck(int ID, string name)
+    {
+        Open();
+        //TODO hav noget confirmation 
+        var cmd = new SqliteCommand($"DELETE FROM Deck WHERE ID={ID} AND Name={name}", (SqliteConnection)connection);
+
+       // cmd.ExecuteNonQuery();
+
+        Close();
+    }
+}
