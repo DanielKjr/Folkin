7ef69edad98ae8f7bf8683a18c118cb78b6e25e8
BrothT3 <79831328+BrothT3@users.noreply.github.com>
2022-11-24 13:24:35 +0100

    CardEditor

---
+++ Assets/Resources/OLD CardStateEditor/CardStates/CardDescriptionState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDescriptionState : CardBaseState
+{
+    public override void EnterState(CardStateManager card)
+    {
+
+    }
+
+    public override void Exit(CardStateManager card)
+    {
+
+    }
+
+    public override void Update(CardStateManager card)
+    {
+
+    }
+}
