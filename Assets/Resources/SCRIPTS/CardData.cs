711146694d2682a65f9d7d1fa326bed39a586c5f
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-11-25 11:27:00 +0100

    CanvasStuff

--- Assets/Resources/SCRIPTS/CardData.cs
+++ Assets/Resources/SCRIPTS/CardData.cs
@@ -1,15 +1,15 @@
 ﻿public class CardData
 {
-    public int DeckID { get; set; }
-    public string TitleText { get; set; }
-    public string DescriptionText { get; set; }
-    public string TypeText { get; set; }
-    public string[] TagText { get; set; }
-    public int[] IconValues { get; set; }
-    public string[] IconPath { get; set; }
-    public string SpritePath { get; set; }
-    public CardType CType { get; set; }
-    public TagType TType { get; set; }
+    public int DeckID { get;  set; }
+    public string TitleText { get; private set; }
+    public string DescriptionText { get; private set; }
+    public string TypeText { get; private set; }
+    public string[] TagText { get; private set; }
+    public int[] IconValues { get; private set; }
+    public string[] IconPath { get; private set; }
+    public string SpritePath { get; private set; }
+    public CardType CType { get; private set; }
+    public TagType TType { get; private set; }
 
     public CardData(string titleText, string description, string cardType, TagType tagType, string[] tagText, string[] iconPath, string sprite)
     {
