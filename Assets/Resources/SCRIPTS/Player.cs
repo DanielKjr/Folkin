b11f687702d567bc847a2f2fbf74a7635f13f726
HCeption <15846@student.eadania.dk>
2022-11-25 14:06:59 +0100

    Multy hand

--- Assets/Resources/SCRIPTS/Player.cs
+++ Assets/Resources/SCRIPTS/Player.cs
@@ -26,7 +26,7 @@ public class Player : MonoBehaviour
         }
         else
         {
-            foreach(var item in hand)
+            foreach (var item in hand)
             {
                 var cardCanvas = item.GetComponentInChildren<Canvas>();
                 cardCanvas.transform.localScale = new Vector3(newScale, newScale, newScale);
@@ -40,14 +40,32 @@ public class Player : MonoBehaviour
     }
     private void AutoShiftCards()
     {
-        int count = hand.Count;
-        int offset = 2; //Offset between each card. Higher number = more space.
+        int offset = 3; //CHANGE OFFSET HERE <-----
+        float count = hand.Count;
         if (count > 1)
         {
-            for (int i = 0; i < count; i++)
+            int x = 0;
+            float y = 0;
+            for (int i = 0; i < (int)count; i++)
             {
-                CardMove(hand[i], new Vector2((i * offset) - count, 0));
+                x++;
+                if (x > 6)
+                {
+                    x = 1;
+                    y -= 2;
+                }
+                //CardMove(hand[i], new Vector2((i * offset) - count + 1, 0));
+                Mover(hand[i], x, offset, (int)y);
             }
+            float scale = 0.01f;
+            float posY = -1f * y;
+            float math = posY / 1000;
+            float mather = scale - math;
+            CardScale(null, mather, true);
         }
+    }
+    private void Mover(Card card, int pos, int offset, int y)
+    {
+        CardMove(card, new Vector2((pos * offset) - 10, y));
     }
 }
